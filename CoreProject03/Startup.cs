8c8858af1ec5231d732616701a531cb07159bec5
fausio luis <fausioluis@live.com>
2020-05-02 22:33:07 +0200

    using  app.UseStaticFiles(); to start the app

--- CoreProject03/Startup.cs
+++ CoreProject03/Startup.cs
@@ -25,10 +25,7 @@ namespace CoreProject03
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
         }
     }
 }
