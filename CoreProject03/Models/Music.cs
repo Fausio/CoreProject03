0b551fa239354f5dc3ab00d47b47bf331525aa9b
fausio luis <fausioluis@live.com>
2020-05-03 18:20:05 +0200

    music model

---
+++ CoreProject03/Models/Music.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject03.Models
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
