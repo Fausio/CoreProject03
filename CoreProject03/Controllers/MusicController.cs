2fd8903272bf589f85fb35c100f9cc1e3957395b
fausio luis <fausioluis@live.com>
2020-05-03 18:19:47 +0200

    <Music controller

---
+++ CoreProject03/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreProject03.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject03.Controllers
+{
+    public class MusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<Music>()
+                        {
+                            new Music{Title= "4 ou 5",Author="Força Suprema ft Prodigio"},
+                            new Music{Title= "Hello",Author="Beyonce"},
+                            new Music{Title= "Patrão",Author="MC - Roger"}
+                        }
+           );
+        }
+    }
+}
