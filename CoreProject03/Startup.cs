9db3bf4bf9015b6678847d1d46dc454e1b46d5ab
fausio luis <fausioluis@live.com>
2020-05-03 12:36:07 +0200

    fix route name

--- CoreProject03/Startup.cs
+++ CoreProject03/Startup.cs
@@ -28,7 +28,7 @@ namespace CoreProject03
             app.UseStaticFiles();
             app.UseMvc(r =>
             {
-                r.MapRoute(name: "defailt", template: "{controller}/{action}/{id}");
+                r.MapRoute(name: "default", template: "{controller}/{action}/{id}");
             });
         }
     }
