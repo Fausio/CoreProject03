46f136c752e0da3215e0ff974c72a96f3ed3b108
fausio luis <fausioluis@live.com>
2020-05-03 18:07:17 +0200

    adding book model

---
+++ CoreProject03/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject03.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
