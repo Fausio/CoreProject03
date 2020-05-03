d4a76841522918543de34ad671f1247602af0d4e
fausio luis <fausioluis@live.com>
2020-05-03 21:05:12 +0200

    band model

---
+++ CoreProject03/Models/Band.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreProject03.Models
+{
+    public class Band
+    {
+        public string Title { get; set; }
+        public string Origin { get; set; }
+        public string Gender { get; set; }
+        public string Biography { get; set; }
+    }
+}
