5a94f210d495f08a52ef07bd775d6fc322fbab61
fausio luis <fausioluis@live.com>
2020-05-04 09:35:32 +0200

    adding  DataAnnotations to band model

--- CoreProject03/Models/Band.cs
+++ CoreProject03/Models/Band.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,16 @@ namespace CoreProject03.Models
 {
     public class Band
     {
+        [Display(Name ="Enter the name of music")]
         public string Title { get; set; }
+
+        [Display(Name = "Enter the name of origin")]
         public string Origin { get; set; }
+
+        [Display(Name = "Enter the name of gender")]
         public string Gender { get; set; }
+
+        [Display(Name = "write a biography")]
         public string Biography { get; set; }
     }
 }
