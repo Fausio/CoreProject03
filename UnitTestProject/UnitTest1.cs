32f0dd7521885aeb678e0740a117bee5ddae8b92
fausio luis <fausioluis@live.com>
2020-05-07 10:02:07 +0200

    Mock Data.

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -1,6 +1,9 @@
 using CoreProject03.Controllers;
-using CoreProject03.Repository; 
+using CoreProject03.Models;
+using CoreProject03.Repository;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
 
 namespace UnitTestProject
 {
@@ -11,9 +14,26 @@ namespace UnitTestProject
         public void ChechIFMusicCOntollerIsNotNull()
         {
             var repository = new RepositoryBand();
-            var controler = new  MusicController(repository);
+            var controler = new MusicController(repository);
 
             Assert.IsNotNull(controler);
+        }
+
+        [TestMethod]
+        public void IfRepositoryBandReturnList()
+        {
+            var moq = new Mock<IRepositoryBand>();
+            var list = moq.Setup(x => x.Bands()).Returns(new List<Band>());
+
+            Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public void IfRepositoryBandReturBand()
+        {
+            var moq = new Mock<IRepositoryBand>();
+          
+            Assert.IsNotNull(moq.Setup(x => x.Band("The moon")).Returns(new Band()));
         }
     }
 }
